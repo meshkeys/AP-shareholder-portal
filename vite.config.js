fbbd3d6a76fa1ec51e7e042aac863dc9a584f645
Meshack <meshkeys@gmail.com>
2026-09-21 12:24:35 +0100

    force env vars into vite config

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,18 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-    historyApiFallback: true,
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  console.log("API URL:", env.VITE_API_URL);
+
+  return {
+    plugins: [react()],
+    server: {
+      port: 3000,
+      historyApiFallback: true,
+    },
+    define: {
+      "import.meta.env.VITE_API_URL": JSON.stringify(env.VITE_API_URL),
+    },
+  };
 });
-
-/*import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-  },
-});*/
