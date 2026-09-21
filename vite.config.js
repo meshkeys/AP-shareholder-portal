65def27e19a5cfebf66739cf62662f07a6b5b141
Meshack <meshkeys@gmail.com>
2026-09-21 12:41:21 +0100

    hardcode Render URL in vite config

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,16 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
-  console.log("API URL:", env.VITE_API_URL);
-
-  return {
-    plugins: [react()],
-    server: {
-      port: 3000,
-      historyApiFallback: true,
-    },
-    define: {
-      "import.meta.env.VITE_API_URL": JSON.stringify(env.VITE_API_URL),
-    },
-  };
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000,
+    historyApiFallback: true,
+  },
+  define: {
+    "import.meta.env.VITE_API_URL": JSON.stringify(
+      "https://ap-shareholder-portal.onrender.com",
+    ),
+    "import.meta.env.VITE_USE_MOCK_OTP": JSON.stringify("false"),
+  },
 });
